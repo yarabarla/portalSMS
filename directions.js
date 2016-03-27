cb7a9033de15398c482661b223416bdb27f1b032
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2016-03-27 13:02:40 -0500

    Snapshot

--- directions.js
+++ directions.js
@@ -1,8 +1,18 @@
 require('dotenv').config();
+const fs = require('fs');
 const Promise = require('bluebird');
 const googleMapsAPI = require('googlemaps');
 
 getDirections('New York City', 'Houston', 'driving').then(res => {
+/*  return res.routes.map(route => route.legs)
+            .map(legs => legs.steps)*/
+//            .map(step => step.instructions)
+  const leg = res.routes[0].legs[0];
+//  return leg.map(x => x.html_instructions)
+  return leg.distance
+//  fs.writeFileSync('obj.txt', JSON.stringify(leg))
+//  return leg
+}).then(res => {
   console.log(res)
 })
 
